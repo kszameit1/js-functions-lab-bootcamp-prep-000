672ad473d49400fa6f7dcded6d80a8a279f440c4
Karen Lowe <k.szameit@mac.com>
2017-10-11 13:20:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,3 +5,7 @@ function happyHolidays() {
 function happyHolidaysTo(name) {
   return `Happy holidays, ${name}!`
 }// write your code below!
+
+function happyHolidayTo(holiday, name) {
+  return `Happy ${holiday}, ${name}!`
+}
